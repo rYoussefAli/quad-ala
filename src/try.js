6a425f3f1f430643a5a2db6b42fad59c264dca27
Youssef Ali <aliyoussef200399@gmail.com>
2022-07-11 04:07:32 +0200

    try function tbd

---
+++ src/try.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function try() {
+  return (
+    <View>
+      <Text>try</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
